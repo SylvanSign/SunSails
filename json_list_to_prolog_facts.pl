d30c5b418aba25334a1136059a8ceedb955f0ab0
sylvansign <jake.d.bray@gmail.com>
2020-12-17 22:20:24 -0500

    initial commit

---
+++ json_list_to_prolog_facts.pl
@@ -0,0 +1,16 @@
+:- use_module(library(http/json)).
+
+:- dynamic set/1.
+
+convert(Functor, InName, OutName) :-
+  open(InName, read, InStream),
+  open(OutName, write, OutStream),
+  json_read_dict(InStream, JsonList, [value_string_as(atom), default_tag(Functor)]),
+  maplist(run(Functor, OutStream), JsonList),
+  close(InStream),
+  close(OutStream).
+
+run(Functor, OutStream, Dict) :-
+  Term =.. [Functor, Dict],
+  writeq(OutStream, Term),
+  writeln(OutStream, .).
